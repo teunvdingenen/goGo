457de47638dbabcc1c2e322e800c608daab02f3a
Teun van Dingenen <teunvdingenen@gmail.com>
2012-09-04 17:07:08 +0200

    Formatting

--- game/game.go
+++ game/game.go
@@ -1,10 +1,10 @@
 package main
 
 import (
-	"goGo/graph"
-	"goGo/gtp"
-	"log"
-	"os"
+    "goGo/graph"
+    "goGo/gtp"
+    "log"
+    "os"
 )
 
 var todo chan []uint8
@@ -16,72 +16,72 @@ var hasPassed bool
 var hasResigned bool
 
 func main() {
-	file, _ := os.Create("logfile")
-	fileLog = log.New(file, "", 0)
-	todo = make(chan []uint8, 32)
-	gtp.Start(todo)
-	watchTodo()
+    file, _ := os.Create("logfile")
+    fileLog = log.New(file, "", 0)
+    todo = make(chan []uint8, 32)
+    gtp.Start(todo)
+    watchTodo()
 }
 
 func watchTodo() {
-	for running {
-		c := <-todo
-		switch c[0] {
-		case 0: //play
-			if c[2] == 254 && c[3] == 254 {
-				hasPassed = true
-				gtp.Respond("", true)
-			} else if c[2] == 255 && c[3] == 255 {
-				hasResigned = true
-				gtp.Respond("", true)
-			} else {
-				graph.UpdateCurrentVertex(c[1], c[2], c[3])
-				gtp.Respond("", true)
-			}
-		case 1: //genmove
-			if hasPassed {
-				gtp.Respond("pass", true)
-			} else {
-			    x, y := graph.GetMove(c[1])
-			    if x == 255 && y == 255 {
-				    gtp.Respond("resign", true)
+    for running {
+        c := <-todo
+        switch c[0] {
+        case 0: //play
+            if c[2] == 254 && c[3] == 254 {
+                hasPassed = true
+                gtp.Respond("", true)
+            } else if c[2] == 255 && c[3] == 255 {
+                hasResigned = true
+                gtp.Respond("", true)
+            } else {
+                graph.UpdateCurrentVertex(c[1], c[2], c[3])
+                gtp.Respond("", true)
+            }
+        case 1: //genmove
+            if hasPassed {
+                gtp.Respond("pass", true)
+            } else {
+                x, y := graph.GetMove(c[1])
+                if x == 255 && y == 255 {
+                    gtp.Respond("resign", true)
                 } else {
                     gtp.Respond(gtp.FromXY(x, y), true)
                 }
-			}
-		case 2: //komi
-			graph.SetKomi(float32(c[1]) * 0.5)
-			gtp.Respond("", true)
-		case 3: //boardsize
+            }
+        case 2: //komi
+            graph.SetKomi(float32(c[1]) * 0.5)
+            gtp.Respond("", true)
+        case 3: //boardsize
             hasPassed = false
             hasResigned = false
-			graph.Initiate(c[1], fileLog)
-			gtp.Respond("", true)
-		case 4: //clearboard
+            graph.Initiate(c[1], fileLog)
+            gtp.Respond("", true)
+        case 4: //clearboard
             hasPassed = false
             hasResigned = false
-			graph.Reset()
-			gtp.Respond("", true)
-		case 5: //quit
-			//TODO make quit function
-			running = false
-			gtp.Respond("", true)
-		case 6: //protocol
-			gtp.Respond("2.0", true)
-		case 7: //name
-			gtp.Respond("goGo", true)
-		case 8: //version
-			gtp.Respond("0.1", true)
-		case 9: //list_commands
-			gtp.Respond(gtp.ListCommands(), true)
-		case 10: //known_commands
-			if c[1] == 0 {
-				gtp.Respond("false", true)
-			} else {
-				gtp.Respond("true", true)
-			}
-		default:
-			gtp.Respond("unknown command", false)
-		}
-	}
+            graph.Reset()
+            gtp.Respond("", true)
+        case 5: //quit
+            //TODO make quit function
+            running = false
+            gtp.Respond("", true)
+        case 6: //protocol
+            gtp.Respond("2.0", true)
+        case 7: //name
+            gtp.Respond("goGo", true)
+        case 8: //version
+            gtp.Respond("0.1", true)
+        case 9: //list_commands
+            gtp.Respond(gtp.ListCommands(), true)
+        case 10: //known_commands
+            if c[1] == 0 {
+                gtp.Respond("false", true)
+            } else {
+                gtp.Respond("true", true)
+            }
+        default:
+            gtp.Respond("unknown command", false)
+        }
+    }
 }
--- graph/graph.go
+++ graph/graph.go
@@ -1,9 +1,9 @@
 package graph
 
 import (
-	"log"
-	"crypto/rand"
-	"time"
+    "crypto/rand"
+    "log"
+    "time"
 )
 
 var topVertex *Vertex
@@ -14,378 +14,378 @@ var komi float32
 var logger *log.Logger
 
 type Vertex struct {
-	whiteWins  uint16
-	blackWins  uint16
-	whiteScore uint8
-	blackScore uint8
-	boardState *Board
-	turn       uint8
+    whiteWins  uint16
+    blackWins  uint16
+    whiteScore uint8
+    blackScore uint8
+    boardState *Board
+    turn       uint8
 
-	plyDepth uint
-	outEdges []*Edge
-	inEdge   *Edge
+    plyDepth uint
+    outEdges []*Edge
+    inEdge   *Edge
 }
 
 type Edge struct {
-	playX uint8
-	playY uint8
+    playX uint8
+    playY uint8
 
-	fromVertex *Vertex
-	toVertex   *Vertex
+    fromVertex *Vertex
+    toVertex   *Vertex
 }
 
 func Initiate(boardSize uint8, log *log.Logger) {
-	logger = log
-	topVertex = new(Vertex)
-	topVertex.boardState = new(Board)
-	topVertex.boardState.Create(uint16(boardSize))
-	topVertex.turn = 1
-	topVertex.plyDepth = 0
-	currentVertex = topVertex
+    logger = log
+    topVertex = new(Vertex)
+    topVertex.boardState = new(Board)
+    topVertex.boardState.Create(uint16(boardSize))
+    topVertex.turn = 1
+    topVertex.plyDepth = 0
+    currentVertex = topVertex
 }
 
 func SetKomi(k float32) {
-	komi = k
+    komi = k
 }
 
 func Reset() {
-	topVertex.boardState.Create(uint16(topVertex.boardState.size))
-	currentVertex = topVertex
+    topVertex.boardState.Create(uint16(topVertex.boardState.size))
+    currentVertex = topVertex
 }
 
 func GetMove(c uint8) (x, y uint8) {
-	createGraph()
-	var bestMove *Edge = nil
-	var biggestDiff uint16 = 0
-	if c != currentVertex.turn {
-		panic("Trying to getmove for wrong color")
-	}
-	if c == 1 {
-		for _, v := range currentVertex.outEdges {
-			if v.toVertex.blackWins > v.toVertex.whiteWins {
-				if diff := v.toVertex.blackWins - v.toVertex.whiteWins; diff > biggestDiff {
-					bestMove = v
-					biggestDiff = diff
-				}
-			}
-		}
-	} else if c == 2 {
-		for _, v := range currentVertex.outEdges {
-			if v.toVertex.blackWins < v.toVertex.whiteWins {
-				if diff := v.toVertex.whiteWins - v.toVertex.blackWins; diff > biggestDiff {
-					bestMove = v
-					biggestDiff = diff
-				}
-			}
-		}
-	} else {
-		panic("Unknown color in GetMove(..)")
-	}
-	if bestMove == nil {
-		return 255, 255
-		//panic("Unable to get a move from graph")
-	}
-	currentVertex.outEdges = []*Edge{bestMove}
+    createGraph()
+    var bestMove *Edge = nil
+    var biggestDiff uint16 = 0
+    if c != currentVertex.turn {
+        panic("Trying to getmove for wrong color")
+    }
+    if c == 1 {
+        for _, v := range currentVertex.outEdges {
+            if v.toVertex.blackWins > v.toVertex.whiteWins {
+                if diff := v.toVertex.blackWins - v.toVertex.whiteWins; diff > biggestDiff {
+                    bestMove = v
+                    biggestDiff = diff
+                }
+            }
+        }
+    } else if c == 2 {
+        for _, v := range currentVertex.outEdges {
+            if v.toVertex.blackWins < v.toVertex.whiteWins {
+                if diff := v.toVertex.whiteWins - v.toVertex.blackWins; diff > biggestDiff {
+                    bestMove = v
+                    biggestDiff = diff
+                }
+            }
+        }
+    } else {
+        panic("Unknown color in GetMove(..)")
+    }
+    if bestMove == nil {
+        return 255, 255
+        //panic("Unable to get a move from graph")
+    }
+    currentVertex.outEdges = []*Edge{bestMove}
 
-	board := new(Board)
+    board := new(Board)
     board.Create(uint16(currentVertex.boardState.size))
     copy(board.s, currentVertex.boardState.s)
-	board.Play(c, bestMove.playX, bestMove.playY)
+    board.Play(c, bestMove.playX, bestMove.playY)
 
-	currentVertex = bestMove.toVertex
-	currentVertex.boardState = board
+    currentVertex = bestMove.toVertex
+    currentVertex.boardState = board
     logger.Printf("My Board is now:\n")
     logger.Printf(board.tostr())
-	return bestMove.playX, bestMove.playY
+    return bestMove.playX, bestMove.playY
 }
 
 func UpdateCurrentVertex(c, x, y uint8) {
-	if c != currentVertex.turn {
-		panic("Updating graph to wrong color's move")
-	}
-	var newCurrent *Vertex = nil
-	for _, v := range currentVertex.outEdges {
-		if v.playX == x && v.playY == y {
-			currentVertex.outEdges = []*Edge{v}
-	        board := new(Board)
+    if c != currentVertex.turn {
+        panic("Updating graph to wrong color's move")
+    }
+    var newCurrent *Vertex = nil
+    for _, v := range currentVertex.outEdges {
+        if v.playX == x && v.playY == y {
+            currentVertex.outEdges = []*Edge{v}
+            board := new(Board)
             board.Create(uint16(currentVertex.boardState.size))
             copy(board.s, currentVertex.boardState.s)
 
-			board.Play(c, x, y)
-			newCurrent = v.toVertex
-			newCurrent.boardState = board
-			break
-		}
-	}
-	if newCurrent == nil {
-		newCurrent = new(Vertex)
-	    board := new(Board)
+            board.Play(c, x, y)
+            newCurrent = v.toVertex
+            newCurrent.boardState = board
+            break
+        }
+    }
+    if newCurrent == nil {
+        newCurrent = new(Vertex)
+        board := new(Board)
         board.Create(uint16(currentVertex.boardState.size))
         copy(board.s, currentVertex.boardState.s)
 
-		newCurrent.boardState = board
+        newCurrent.boardState = board
 
-		score, _ := newCurrent.boardState.Play(c, x, y)
+        score, _ := newCurrent.boardState.Play(c, x, y)
 
-		if c == 1 {
-			newCurrent.blackScore += score
-			newCurrent.turn = 2
-		} else {
-			newCurrent.whiteScore += score
-			newCurrent.turn = 1
-		}
-		newCurrent.plyDepth = currentVertex.plyDepth + 1
+        if c == 1 {
+            newCurrent.blackScore += score
+            newCurrent.turn = 2
+        } else {
+            newCurrent.whiteScore += score
+            newCurrent.turn = 1
+        }
+        newCurrent.plyDepth = currentVertex.plyDepth + 1
 
-		newEdge := new(Edge)
-		newEdge.fromVertex = currentVertex
-		newEdge.toVertex = newCurrent
+        newEdge := new(Edge)
+        newEdge.fromVertex = currentVertex
+        newEdge.toVertex = newCurrent
 
-		newEdge.playX = x
-		newEdge.playY = y
+        newEdge.playX = x
+        newEdge.playY = y
 
-		currentVertex.outEdges = []*Edge{newEdge}
-		newCurrent.inEdge = newEdge
-	}
-	currentVertex = newCurrent
+        currentVertex.outEdges = []*Edge{newEdge}
+        newCurrent.inEdge = newEdge
+    }
+    currentVertex = newCurrent
     logger.Printf("My Board is now:\n")
     logger.Printf(newCurrent.boardState.tostr())
 }
 
 func createGraph() {
-	//	maxMoves := uint(currentVertex.boardState.size * currentVertex.boardState.size)
-	doUntil := time.Now().Add(10 * time.Second)
-	for time.Now().Before(doUntil) {
-		toDepth := 80 + currentVertex.plyDepth + 1
-		_ = doRoutine(currentVertex, toDepth)
-	}
+    //  maxMoves := uint(currentVertex.boardState.size * currentVertex.boardState.size)
+    doUntil := time.Now().Add(10 * time.Second)
+    for time.Now().Before(doUntil) {
+        toDepth := 80 + currentVertex.plyDepth + 1
+        _ = doRoutine(currentVertex, toDepth)
+    }
 }
 
 func doRoutine(fromVertex *Vertex, toDepth uint) bool {
-	if toDepth == fromVertex.plyDepth {
-		return true
-	}
+    if toDepth == fromVertex.plyDepth {
+        return true
+    }
 
-	var x, y uint8
-	var score uint8
-	var board *Board
-	err := "error"
+    var x, y uint8
+    var score uint8
+    var board *Board
+    err := "error"
 
     board = new(Board)
     board.Create(uint16(currentVertex.boardState.size))
     copy(board.s, fromVertex.boardState.s)
 
-	possibilities := len(board.GetEmpty())
-	i := 0
+    possibilities := len(board.GetEmpty())
+    i := 0
 
-	for err != "" {
-		x, y = getRandomMove(board)
-		for _, v := range fromVertex.outEdges {
-			if x == v.playX && y == v.playY {
-				_, _ = board.Play(fromVertex.turn, x, y)
-				v.toVertex.boardState = board
-				return doRoutine(v.toVertex, toDepth)
-			}
-		}
+    for err != "" {
+        x, y = getRandomMove(board)
+        for _, v := range fromVertex.outEdges {
+            if x == v.playX && y == v.playY {
+                _, _ = board.Play(fromVertex.turn, x, y)
+                v.toVertex.boardState = board
+                return doRoutine(v.toVertex, toDepth)
+            }
+        }
 
-		score, err = board.Play(fromVertex.turn, x, y)
+        score, err = board.Play(fromVertex.turn, x, y)
 
-		if fromVertex.plyDepth > 2 {
+        if fromVertex.plyDepth > 2 {
             koCompare := fromVertex.inEdge.fromVertex.boardState
-			if board.IsEqual(koCompare) {
-				err = "KO"
-			}
-		}
-		if err != "" {
-			board.Remove(x, y)
-			i += 1
-		}
-		if possibilities == i {
-			return false
-		}
-	}
+            if board.IsEqual(koCompare) {
+                err = "KO"
+            }
+        }
+        if err != "" {
+            board.Remove(x, y)
+            i += 1
+        }
+        if possibilities == i {
+            return false
+        }
+    }
 
-	newVertex := new(Vertex)
+    newVertex := new(Vertex)
 
-	newEdge := new(Edge)
-	newEdge.playX = x
-	newEdge.playY = y
-	newEdge.fromVertex = fromVertex
-	newEdge.toVertex = newVertex
+    newEdge := new(Edge)
+    newEdge.playX = x
+    newEdge.playY = y
+    newEdge.fromVertex = fromVertex
+    newEdge.toVertex = newVertex
 
-	fromVertex.outEdges = append(fromVertex.outEdges, newEdge)
-	newVertex.inEdge = newEdge
+    fromVertex.outEdges = append(fromVertex.outEdges, newEdge)
+    newVertex.inEdge = newEdge
 
-	newVertex.boardState = board
-	if fromVertex.turn == 1 {
-		newVertex.turn = 2
-		newVertex.blackScore += score
-	} else {
-		newVertex.turn = 1
-		newVertex.whiteScore += score
-	}
-	newVertex.plyDepth = fromVertex.plyDepth + 1
+    newVertex.boardState = board
+    if fromVertex.turn == 1 {
+        newVertex.turn = 2
+        newVertex.blackScore += score
+    } else {
+        newVertex.turn = 1
+        newVertex.whiteScore += score
+    }
+    newVertex.plyDepth = fromVertex.plyDepth + 1
 
-	if newVertex.plyDepth > uint(board.size*board.size/2) {
-		endBlack, endWhite := scoreBoardOld(newVertex)
-		endBlack += newVertex.blackScore
-		endWhite += newVertex.whiteScore + uint8(komi-0.5)
-		if endBlack > endWhite {
-			vertex := newVertex
-			for vertex != currentVertex {
-				vertex.blackWins += 1
-				vertex = vertex.inEdge.fromVertex
-			}
-		} else if endBlack <= endWhite {
-			vertex := newVertex
-			for vertex != currentVertex {
-				vertex.whiteWins += 1
-				vertex = vertex.inEdge.fromVertex
-			}
-		}
-	}
-	return doRoutine(newVertex, toDepth)
+    if newVertex.plyDepth > uint(board.size*board.size/2) {
+        endBlack, endWhite := scoreBoardOld(newVertex)
+        endBlack += newVertex.blackScore
+        endWhite += newVertex.whiteScore + uint8(komi-0.5)
+        if endBlack > endWhite {
+            vertex := newVertex
+            for vertex != currentVertex {
+                vertex.blackWins += 1
+                vertex = vertex.inEdge.fromVertex
+            }
+        } else if endBlack <= endWhite {
+            vertex := newVertex
+            for vertex != currentVertex {
+                vertex.whiteWins += 1
+                vertex = vertex.inEdge.fromVertex
+            }
+        }
+    }
+    return doRoutine(newVertex, toDepth)
 }
 
 func getRandomMove(b *Board) (x, y uint8) {
-	empty := b.GetEmpty()
-	bs := make([]byte, 8)
-	_, _ = rand.Read(bs)
-	var i int = 0
-	for _, v := range bs {
-		i += int(v)
-	}
-	i = i % len(empty)
-	if i%2 == 0 { //index is x
-		x = empty[i]
-		y = empty[i+1]
-	} else { //index is y
-		y = empty[i]
-		x = empty[i-1]
-	}
-	return x, y
+    empty := b.GetEmpty()
+    bs := make([]byte, 8)
+    _, _ = rand.Read(bs)
+    var i int = 0
+    for _, v := range bs {
+        i += int(v)
+    }
+    i = i % len(empty)
+    if i%2 == 0 { //index is x
+        x = empty[i]
+        y = empty[i+1]
+    } else { //index is y
+        y = empty[i]
+        x = empty[i-1]
+    }
+    return x, y
 }
 
 func scoreBoard(v *Vertex) (scoreBlack, scoreWhite uint8) {
-	scoreBlack = 0
-	scoreWhite = 0
-	processedMatrix := make([]uint8, v.boardState.size*v.boardState.size)
-	b := v.boardState
+    scoreBlack = 0
+    scoreWhite = 0
+    processedMatrix := make([]uint8, v.boardState.size*v.boardState.size)
+    b := v.boardState
 
-	for i, v := range processedMatrix {
-		if v == 1 {
-			continue
-		}
-		x, y := calcXY(i, b.size)
-		if b.GetColor(x, y) == 0 {
-			xa, xb, ya, yb := GetAdjecent(x, y)
-			adjecentColor := uint8(0)
-			found2Colors := false
-			if xa < b.size && b.GetColor(xa, y) != adjecentColor {
-				if adjecentColor == 0 {
-					adjecentColor = b.GetColor(xa, y)
-				} else {
-					found2Colors = true
-				}
-			}
-			if xb < b.size && b.GetColor(xb, y) != adjecentColor {
-				if adjecentColor == 0 {
-					adjecentColor = b.GetColor(xb, y)
-				} else {
-					found2Colors = true
-				}
-			}
-			if ya < b.size && b.GetColor(x, ya) != adjecentColor {
-				if adjecentColor == 0 {
-					adjecentColor = b.GetColor(x, ya)
-				} else {
-					found2Colors = true
-				}
-			}
-			if yb < b.size && b.GetColor(x, yb) != adjecentColor {
-				if adjecentColor == 0 {
-					adjecentColor = b.GetColor(x, yb)
-				} else {
-					found2Colors = true
-				}
-			}
-			if !found2Colors && adjecentColor == 1 {
-				scoreBlack += 1
-			} else if !found2Colors && adjecentColor == 2 {
-				scoreWhite += 1
-			}
-		}
-	}
-	logger.Printf("Scored Board (b,w): (%d,%d)\n", scoreBlack, scoreWhite)
-	logger.Printf(b.tostr())
-	return scoreBlack, scoreWhite
+    for i, v := range processedMatrix {
+        if v == 1 {
+            continue
+        }
+        x, y := calcXY(i, b.size)
+        if b.GetColor(x, y) == 0 {
+            xa, xb, ya, yb := GetAdjecent(x, y)
+            adjecentColor := uint8(0)
+            found2Colors := false
+            if xa < b.size && b.GetColor(xa, y) != adjecentColor {
+                if adjecentColor == 0 {
+                    adjecentColor = b.GetColor(xa, y)
+                } else {
+                    found2Colors = true
+                }
+            }
+            if xb < b.size && b.GetColor(xb, y) != adjecentColor {
+                if adjecentColor == 0 {
+                    adjecentColor = b.GetColor(xb, y)
+                } else {
+                    found2Colors = true
+                }
+            }
+            if ya < b.size && b.GetColor(x, ya) != adjecentColor {
+                if adjecentColor == 0 {
+                    adjecentColor = b.GetColor(x, ya)
+                } else {
+                    found2Colors = true
+                }
+            }
+            if yb < b.size && b.GetColor(x, yb) != adjecentColor {
+                if adjecentColor == 0 {
+                    adjecentColor = b.GetColor(x, yb)
+                } else {
+                    found2Colors = true
+                }
+            }
+            if !found2Colors && adjecentColor == 1 {
+                scoreBlack += 1
+            } else if !found2Colors && adjecentColor == 2 {
+                scoreWhite += 1
+            }
+        }
+    }
+    logger.Printf("Scored Board (b,w): (%d,%d)\n", scoreBlack, scoreWhite)
+    logger.Printf(b.tostr())
+    return scoreBlack, scoreWhite
 }
 
 func calcXY(index int, size uint8) (x, y uint8) {
-	i := uint8(index)
-	y = i / size
-	x = i % size
-	return x, y
+    i := uint8(index)
+    y = i / size
+    x = i % size
+    return x, y
 }
 
 func scoreBoardOld(v *Vertex) (scoreBlack, scoreWhite uint8) {
-	b := v.boardState
-	empty := b.GetEmpty()
+    b := v.boardState
+    empty := b.GetEmpty()
 
-	for len(empty) > 0 {
-		xs := []uint8{empty[0]}
-		ys := []uint8{empty[1]}
-		b.GetGroup(0, 0, xs, ys)
+    for len(empty) > 0 {
+        xs := []uint8{empty[0]}
+        ys := []uint8{empty[1]}
+        b.GetGroup(0, 0, xs, ys)
 
-		var adjecentColor uint8 = 0
-		found2Colors := false
-		for i, x := range xs {
-			xa, xb, ya, yb := GetAdjecent(x, ys[i])
-			if xa < b.size && b.GetColor(xa, ys[i]) != adjecentColor {
-				if adjecentColor == 0 {
-					adjecentColor = b.GetColor(xa, ys[i])
-				} else {
-					found2Colors = true
-					break
-				}
-			}
-			if xb < b.size && b.GetColor(xb, ys[i]) != adjecentColor {
-				if adjecentColor == 0 {
-					adjecentColor = b.GetColor(xb, ys[i])
-				} else {
-					found2Colors = true
-					break
-				}
-			}
-			if ya < b.size && b.GetColor(x, ya) != adjecentColor {
-				if adjecentColor == 0 {
-					adjecentColor = b.GetColor(x, ya)
-				} else {
-					found2Colors = true
-					break
-				}
-			}
-			if yb < b.size && b.GetColor(x, yb) != adjecentColor {
-				if adjecentColor == 0 {
-					adjecentColor = b.GetColor(x, yb)
-				} else {
-					found2Colors = true
-					break
-				}
-			}
-		}
-		if !found2Colors && adjecentColor == 1 {
-			scoreBlack += uint8(len(xs))
-		} else if !found2Colors && adjecentColor == 2 {
-			scoreWhite += uint8(len(xs))
-		} 
+        var adjecentColor uint8 = 0
+        found2Colors := false
+        for i, x := range xs {
+            xa, xb, ya, yb := GetAdjecent(x, ys[i])
+            if xa < b.size && b.GetColor(xa, ys[i]) != adjecentColor {
+                if adjecentColor == 0 {
+                    adjecentColor = b.GetColor(xa, ys[i])
+                } else {
+                    found2Colors = true
+                    break
+                }
+            }
+            if xb < b.size && b.GetColor(xb, ys[i]) != adjecentColor {
+                if adjecentColor == 0 {
+                    adjecentColor = b.GetColor(xb, ys[i])
+                } else {
+                    found2Colors = true
+                    break
+                }
+            }
+            if ya < b.size && b.GetColor(x, ya) != adjecentColor {
+                if adjecentColor == 0 {
+                    adjecentColor = b.GetColor(x, ya)
+                } else {
+                    found2Colors = true
+                    break
+                }
+            }
+            if yb < b.size && b.GetColor(x, yb) != adjecentColor {
+                if adjecentColor == 0 {
+                    adjecentColor = b.GetColor(x, yb)
+                } else {
+                    found2Colors = true
+                    break
+                }
+            }
+        }
+        if !found2Colors && adjecentColor == 1 {
+            scoreBlack += uint8(len(xs))
+        } else if !found2Colors && adjecentColor == 2 {
+            scoreWhite += uint8(len(xs))
+        }
 
-		for i := 0; i < len(empty); i += 2 {
-			emptyx := empty[i]
-			emptyy := empty[i+1]
-			if IsPresentinGroup(emptyx, emptyy, xs, ys) {
-				empty = append(empty[:i], empty[i+2:]...)
-			}
-		}
-	}
-	return scoreBlack, scoreWhite
+        for i := 0; i < len(empty); i += 2 {
+            emptyx := empty[i]
+            emptyy := empty[i+1]
+            if IsPresentinGroup(emptyx, emptyy, xs, ys) {
+                empty = append(empty[:i], empty[i+2:]...)
+            }
+        }
+    }
+    return scoreBlack, scoreWhite
 }
